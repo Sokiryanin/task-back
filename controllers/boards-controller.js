11ed03557d78300cd236b1faa748368df92c0fea
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2024-04-23 19:58:28 +0200

    add decorateros&update controllers

--- controllers/boards-controller.js
+++ controllers/boards-controller.js
@@ -1,157 +1,95 @@
 import { HttpError } from "../helpers/index.js";
 import boardService from "../models/boards/index.js";
-import {
-  boardAddSchema,
-  boardAddTaskSchema,
-  boardUpdateSchema,
-  boardUpdateTaskSchema,
-} from "../schemas/boardsSchemas.js";
+import { ctrlWrapper } from "../decorators/index.js";
 
 /* Boards */
-
-const getBoards = async (req, res, next) => {
-  try {
-    const result = await boardService.getAllBoards();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const getBoards = async (req, res) => {
+  const result = await boardService.getAllBoards();
+  res.json(result);
 };
 
-const getBoardById = async (req, res, next) => {
-  try {
-    // req.params - получаем все динамические параметры из url.
+const getBoardById = async (req, res) => {
+  // req.params - получаем все динамические параметры из url.
+  const { id } = req.params;
+  const result = await boardService.getBoardById(id);
 
-    const { id } = req.params;
-    const result = await boardService.getBoardById(id);
-
-    if (!result) {
-      throw HttpError(404, `Board with id=${id} not found`);
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, `Board with id=${id} not found`);
   }
+  res.json(result);
 };
 
-const createBoard = async (req, res, next) => {
-  try {
-    /* нам потрібно передати тіло запиту, воно знаходитсья в req.body
+const createBoard = async (req, res) => {
+  /* нам потрібно передати тіло запиту, воно знаходитсья в req.body
 console.log(req.body); */
 
-    // Диструктуризуємо error
-    const { error } = boardAddSchema.validate(req.body);
-
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await boardService.addBoard(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+  const result = await boardService.addBoard(req.body);
+  res.status(201).json(result);
 };
 
-const updateByIdBoard = async (req, res, next) => {
-  try {
-    const { error } = boardUpdateSchema.validate(req.body);
-
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    // якщо все ок то забираємо id з req.params
-    const { id } = req.params;
-
-    // передаємо в наш метод id і req.body
-    const result = await boardService.updateBoardById(id, req.body);
-    // потрібно обробити якщо отримаємо null
-    if (!result) {
-      throw HttpError(404, `Board with id=${id} not found`);
-    }
-
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateByIdBoard = async (req, res) => {
+  // якщо все ок то забираємо id з req.params
+  const { id } = req.params;
+  // передаємо в наш метод id і req.body
+  const result = await boardService.updateBoardById(id, req.body);
+  // потрібно обробити якщо отримаємо null
+  if (!result) {
+    throw HttpError(404, `Board with id=${id} not found`);
   }
+  res.json(result);
 };
 
-const deleteByIdBoard = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await boardService.deleteBoardById(id);
+const deleteByIdBoard = async (req, res) => {
+  const { id } = req.params;
+  const result = await boardService.deleteBoardById(id);
 
-    if (!result) {
-      throw HttpError(404, `Board with id=${id} not found`);
-    }
-    res.json(result);
-    // res.status(204).send();
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(404, `Board with id=${id} not found`);
   }
+  res.json(result);
+  // res.status(204).send();
 };
 
 /* Tasks */
 
-const createNewTask = async (req, res, next) => {
-  try {
-    const { error } = boardAddTaskSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-
-    const result = await boardService.addTaskToBoard(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+const createNewTask = async (req, res) => {
+  const result = await boardService.addTaskToBoard(req.body);
+  res.status(201).json(result);
 };
 
-const updateTaskById = async (req, res, next) => {
-  try {
-    const { error } = boardUpdateTaskSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id, taskId } = req.params;
-    const result = await boardService.updateTaskInBoard(id, taskId, req.body);
-    if (!result) {
-      throw HttpError(
-        404,
-        `Task with id=${taskId} in board with id=${id} not found`
-      );
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+const updateTaskById = async (req, res) => {
+  const { id, taskId } = req.params;
+  const result = await boardService.updateTaskInBoard(id, taskId, req.body);
+  if (!result) {
+    throw HttpError(
+      404,
+      `Task with id=${taskId} in board with id=${id} not found`
+    );
   }
+  res.json(result);
 };
 
-const deleteTaskById = async (req, res, next) => {
-  try {
-    const { taskId } = req.params;
-    const result = await boardService.deleteTaskFromBoard(taskId); // Передаем taskId напрямую
-    console.log(result);
+const deleteTaskById = async (req, res) => {
+  const { taskId } = req.params;
+  const result = await boardService.deleteTaskFromBoard(taskId); // Передаем taskId напрямую
+  console.log(result);
 
-    if (!result) {
-      throw HttpError(
-        404,
-        `Task with id=${taskId} in board with id=${id} not found`
-      );
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
+  if (!result) {
+    throw HttpError(
+      404,
+      `Task with id=${taskId} in board with id=${id} not found`
+    );
   }
+  res.json(result);
 };
 
 export default {
-  getBoards,
-  getBoardById,
-  createBoard,
-  updateByIdBoard,
-  deleteByIdBoard,
-  createNewTask,
-  updateTaskById,
-  deleteTaskById,
+  getBoards: ctrlWrapper(getBoards),
+  getBoardById: ctrlWrapper(getBoardById),
+  createBoard: ctrlWrapper(createBoard),
+  updateByIdBoard: ctrlWrapper(updateByIdBoard),
+  deleteByIdBoard: ctrlWrapper(deleteByIdBoard),
+  createNewTask: ctrlWrapper(createNewTask),
+  updateTaskById: ctrlWrapper(updateTaskById),
+  deleteTaskById: ctrlWrapper(deleteTaskById),
 };
--- routes/api/boards-router.js
+++ routes/api/boards-router.js
@@ -1,26 +1,53 @@
 import express from "express";
-
 import boardsController from "../../controllers/boards-controller.js";
-
 import { isEmptyBody } from "../../middlewares/index.js";
+
+import {
+  boardAddSchema,
+  boardAddTaskSchema,
+  boardUpdateSchema,
+  boardUpdateTaskSchema,
+} from "../../schemas/boardsSchemas.js";
+
+import { validateBody } from "../../decorators/index.js";
 
 const boardsRouter = express.Router();
 
 /* Boards */
 boardsRouter.get("/", boardsController.getBoards);
+
 boardsRouter.get("/:id", boardsController.getBoardById);
-boardsRouter.post("/", isEmptyBody, boardsController.createBoard);
-boardsRouter.put("/:id", isEmptyBody, boardsController.updateByIdBoard);
+
+boardsRouter.post(
+  "/",
+  isEmptyBody,
+  validateBody(boardAddSchema),
+  boardsController.createBoard
+);
+
+boardsRouter.put(
+  "/:id",
+  isEmptyBody,
+  validateBody(boardUpdateSchema),
+  boardsController.updateByIdBoard
+);
+
 boardsRouter.delete("/:id", boardsController.deleteByIdBoard);
 
 /* Tasks */
-boardsRouter.post("/:id/tasks", isEmptyBody, boardsController.createNewTask);
+boardsRouter.post(
+  "/:id/tasks",
+  isEmptyBody,
+  validateBody(boardAddTaskSchema),
+  boardsController.createNewTask
+);
+
 boardsRouter.put(
   "/:id/tasks/:taskId",
   isEmptyBody,
+  validateBody(boardUpdateTaskSchema),
   boardsController.updateTaskById
 );
 boardsRouter.delete("/:id/tasks/:taskId", boardsController.deleteTaskById);
-
 
 export default boardsRouter;
