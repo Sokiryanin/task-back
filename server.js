865574caf75266828ece5795485d45e1329faec6
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2024-04-24 15:20:47 +0200

    add db to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import app from "./app.js";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://oldLocman:Ekau4jwf7UZUPT8e@cluster0.jzgqpwa.mongodb.net/my-boards?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+
