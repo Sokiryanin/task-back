e10237732f1c9f8cdd1d417df961f563e7a8452b
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2024-04-24 22:12:22 +0200

    fix bugs

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import app from "./app.js";
 import mongoose from "mongoose";
 
 const { DB_HOST } = process.env;
-
+console.log(DB_HOST);
 mongoose
-  .connect(DB_HOST)
+  .connect(
+    "mongodb+srv://oldLocman:Ekau4jwf7UZUPT8e@cluster0.jzgqpwa.mongodb.net/my-boards?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
